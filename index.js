059947c3408746d9527cd8d7f4818ef74f5e8782
YesilankaManikanta <pradeepchandu88@gmail.com>
2025-07-08 21:16:22 +0530

    add comment again

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-// mongodb database connection in backend 
+// mongodb database connection in backend form nodejs
 
 async function connectToDatabase() {
     try {
