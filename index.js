3cbd59d7939d06e7327ade80e338b9136294651d
YesilankaManikanta <pradeepchandu88@gmail.com>
2025-07-08 20:56:03 +0530

    just add commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-// mongodb database connection 
+// mongodb database connection in backend 
 
 async function connectToDatabase() {
     try {
