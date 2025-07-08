a4ca84572ad9bec90c9c688e4cdf4e1c8b4a94d3
Yesilanka Manikanta <pradeepchandu88@gmail.com>
2025-07-08 22:49:36 +0530

    change email

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-// mongodb database connection  config --global user.email
+// mongodb database connection  change mail
 
 
 async function connectToDatabase() {
